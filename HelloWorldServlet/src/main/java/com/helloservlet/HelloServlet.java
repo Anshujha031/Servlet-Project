aaecea11dd2d978eacfe6bcfed33d975eac9170c
naveenjha031 <naveenjha344@gmail.com>
2025-02-21 13:25:51 +0530

    [Naveen] Add. Hello World Program By Servlet

---
+++ HelloWorldServlet/src/main/java/com/helloservlet/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.helloservlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello") // URL Mapping
+public class HelloServlet extends HttpServlet{
+
+
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<h1>Hello, World! Welcome to Servlets</h1>");
+        }
+    }
+
